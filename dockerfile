ad77e24ad20aeed094fa29979e444adb8d80ffba
Jan Hoek <github@uncommonsense.nl>
2018-12-19 23:23:56 +0100

    Initial commit.

---
+++ dockerfile
@@ -0,0 +1,2 @@
+FROM peterdemin/kibitzr
+RUN apt-get update; apt-get install -y curl
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# docker run -v $PWD:/root/.config/kibitzr -v $PWD/pages:/pages peterdemin/kibitzr run
+docker run -v $PWD:/root/.config/kibitzr -v $PWD/pages:/pages 53c4f39296db run
