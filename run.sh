2639c876c4b4bf1372780a10bce22dbd6abb97f8
Jan Hoek <github@uncommonsense.nl>
2018-12-21 17:14:15 +0100

    Runner script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # docker run -v $PWD:/root/.config/kibitzr -v $PWD/pages:/pages peterdemin/kibitzr run
-docker run -d --rm -v $PWD:/root/.config/kibitzr -v $PWD/pages:/pages 34d68389d6b5 run
+docker run -d --rm -v $PWD:/root/.config/kibitzr -v $PWD/pages:/pages 53c4f39296db run
