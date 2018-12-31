92b53fef9fd564d51b548b72a5560ac60a41adfd
Jan Hoek <github@uncommonsense.nl>
2018-12-31 14:49:20 +0100

    Made sudoku script smarter

--- mykibitzr/sudoku/script.ps1
+++ mykibitzr/sudoku/script.ps1
@@ -1,18 +1,18 @@
 while ($true)
 {
-    $NoScript =
-    (Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' |
-            Select-Object -ExpandProperty Content |
-            pup --plain '.nmt-layout .nmt-item:first-of-type noscript') -join ' '
-
+    $Content = Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' | Select-Object -ExpandProperty Content
+    $NoScript = ($Content | pup --plain '.nmt-layout .nmt-item:first-of-type noscript') -join ' '
     $Link = [regex]::Match($NoScript, 'src="(.*?)"').Groups[1].Value
+    $Hyperlink = ($Content | pup '.nmt-layout .nmt-item:first-of-type a attr{href}')
+    $Date = (($HyperLink -split '/')[2..4]) -join '.'
 
     Send-PushoverNotification `
         -ApplicationToken av5b3yspt9nzrphgdpq135fa64do6j `
         -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
         -Title Sudoku `
-        -Message  `
-        -SupplementaryUrl $Link
+        -Message $Date `
+        -SupplementaryUrl $Link `
+        -SupplementaryUrlTitle Link
 
     Start-Sleep -Seconds (60 * 60 * 24)
 }
