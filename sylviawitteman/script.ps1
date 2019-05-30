ab2408d6622001852d75ac906c82a451a0bd5a3e
Jan Hoek <github@uncommonsense.nl>
2019-05-30 20:25:38 +0200

    More work on sylviawitteman

--- sylviawitteman/script.ps1
+++ sylviawitteman/script.ps1
@@ -1,12 +1,11 @@
-while ($true)
-{
-    $Index = Invoke-WebRequest -Uri https://www.volkskrant.nl/search?query=sylvia+witteman |
-        Select-Object -ExpandProperty Content |
-        pup 'article a attr{href}' |
-        Where-Object { $_ -like '/mensen/*' } |
-        ForEach-Object { "https://volkskrant.nl$($_)" }
+Invoke-WebRequest -Uri https://www.volkskrant.nl/auteur/Sylvia%20Witteman `
+| Select-Object -ExpandProperty Content `
+| pup 'article a attr{href}' `
+| Select-Object -First 1 `
+| ForEach-Object {     
+    $Content = Invoke-WebRequest -Uri "https://volkskrant.nl$($_)" `
+    | Select-Object -ExpandProperty Content
 
-    $Index
-
-    Start-Sleep -Seconds (60 * 60 * 24)
+    $Heading = $Content | pup 'h1 text{}'
+    $Content | pup 'section.artstyle__main--container p' | ForEach-Object { $_; '' }
 }
