39ff9787f1a3d95fc0da50f89d409b193e6b4679
Jan Hoek <github@uncommonsense.nl>
2019-06-04 22:12:32 +0200

    Restructured script

--- ikjes/script.ps1
+++ ikjes/script.ps1
@@ -1,12 +1,27 @@
 $ProgressPreference = 'SilentlyContinue'
 
-$Index = Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/ikje |
-    Select-Object -ExpandProperty Content |
-    ./pup 'a.nmt-item__link attr{href}'
-$Content = Invoke-WebRequest -Uri "https://www.nrc.nl$($Index[0])" |
-    Select-Object -ExpandProperty Content
-$Title = $Content | ./pup 'h1[data-flowtype="headline"] text{}'
-$Body = ($Content | ./pup 'div.content p text{}') -join ' '
+$Result = New-Object -TypeName System.Collections.ArrayList
 
-$Title
-$Body
+Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/ikje `
+    | Select-Object -ExpandProperty Content `
+    | ./pup 'a.nmt-item__link attr{href}' `
+    | Select-Object -First 10 `
+    | ForEach-Object {
+    $Url = "https://www.nrc.nl$($_)"
+    Invoke-WebRequest -Uri $Url `
+        | Select-Object -ExpandProperty Content `
+        | ForEach-Object {
+        $DateText = (($Url -split '/')[4..6]) -join '-'
+
+        $Properties = [Ordered]@{
+            Date  = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
+            Title = ($_ | ./pup 'h1[data-flowtype="headline"] text{}')
+            Body  = ($_ | ./pup 'div.content p text{}') -join ' '
+            Url   = $Url
+        }
+
+        $Result.Add($Properties) | Out-Null
+    }
+}
+
+$Result | ConvertTo-Json
