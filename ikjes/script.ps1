75fef200a4b55d8701f65f18f0863b0666c3d8fe
Jan Hoek <github@uncommonsense.nl>
2019-06-04 21:41:19 +0200

    Do not loop; hide progress text

--- ikjes/script.ps1
+++ ikjes/script.ps1
@@ -1,14 +1,12 @@
-while ($true)
-{
-    $Index = Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/ikje |
-        Select-Object -ExpandProperty Content |
-        ./pup 'a.nmt-item__link attr{href}'
-    $Content = Invoke-WebRequest -Uri "https://www.nrc.nl$($Index[0])" |
-        Select-Object -ExpandProperty Content
-    $Title = $Content | ./pup 'h1[data-flowtype="headline"] text{}'
-    $Body = ($Content | ./pup 'div.content p text{}') -join ' '
+$ProgressPreference = 'SilentlyContinue'
 
-    Send-PushoverNotification -ApplicationToken afdyp3jviqdcc19qq8qytb4y9p9ojs -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp -Title $Title -Message $Body
+$Index = Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/ikje |
+    Select-Object -ExpandProperty Content |
+    ./pup 'a.nmt-item__link attr{href}'
+$Content = Invoke-WebRequest -Uri "https://www.nrc.nl$($Index[0])" |
+    Select-Object -ExpandProperty Content
+$Title = $Content | ./pup 'h1[data-flowtype="headline"] text{}'
+$Body = ($Content | ./pup 'div.content p text{}') -join ' '
 
-    Start-Sleep -Seconds (60 * 60 * 24)
-}
+$Title
+$Body
