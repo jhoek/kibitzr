41fefdcda3507acd8e1c7e5907514252068597bf
Jan Hoek <github@uncommonsense.nl>
2019-06-05 14:46:16 +0200

    Send results to AirTable

--- ikjes/script.ps1
+++ ikjes/script.ps1
@@ -1,27 +1,26 @@
 $ProgressPreference = 'SilentlyContinue'
-
-$Result = New-Object -TypeName System.Collections.ArrayList
+$SmartSingleQuotes = '[\u2019\u2018\u201A]'
+$SmartDoubleQuotes = '[\u201C\u201D\u201E]'
 
 Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/ikje `
     | Select-Object -ExpandProperty Content `
-    | ./pup 'a.nmt-item__link attr{href}' `
+    | pup 'a.nmt-item__link attr{href}' `
     | Select-Object -First 10 `
     | ForEach-Object {
     $Url = "https://www.nrc.nl$($_)"
-    Invoke-WebRequest -Uri $Url `
-        | Select-Object -ExpandProperty Content `
-        | ForEach-Object {
-        $DateText = (($Url -split '/')[4..6]) -join '-'
+    $DateText = (($Url -split '/')[4..6]) -join '-'
+    $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
+    $Title = ($Content | pup 'h1[data-flowtype="headline"] text{}')
+    $Body = ((($Content | pup 'div.content p text{}') -join ' ') -replace $SmartSingleQuotes, '''') -replace $SmartDoubleQuotes, '"'
+    $Date = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
+    $Payload = (@{fields = @{ Url = $Url; Title = $Title; Body = $Body; Date = $Date }} | ConvertTo-Json -Depth 5)
 
-        $Properties = [Ordered]@{
-            Date  = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
-            Title = ($_ | ./pup 'h1[data-flowtype="headline"] text{}')
-            Body  = ($_ | ./pup 'div.content p text{}') -join ' '
-            Url   = $Url
-        }
+    Write-Verbose $Payload
 
-        $Result.Add($Properties) | Out-Null
-    }
+    Invoke-RestMethod `
+        -Method POST `
+        -Uri 'https://api.airtable.com/v0/appB4Jzod47gLXUVE/ikjes' `
+        -ContentType 'application/json' `
+        -Headers @{'Authorization' = "Bearer $($env:AirTableApiKey)"} `
+        -Body $Payload
 }
-
-$Result | ConvertTo-Json
