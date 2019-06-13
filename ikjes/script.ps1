32bbce7376370134175aed850f71d6707e6f1fef
Jan Hoek <github@uncommonsense.nl>
2019-06-13 16:43:57 +0200

    Skip if already exists

--- ikjes/script.ps1
+++ ikjes/script.ps1
@@ -3,24 +3,25 @@ $SmartSingleQuotes = '[\u2019\u2018\u201A]'
 $SmartDoubleQuotes = '[\u201C\u201D\u201E]'
 
 Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/ikje `
-    | Select-Object -ExpandProperty Content `
-    | pup 'a.nmt-item__link attr{href}' `
-    | Select-Object -First 10 `
-    | ForEach-Object {
+| Select-Object -ExpandProperty Content `
+| pup 'a.nmt-item__link attr{href}' `
+| Select-Object -First 10 `
+| ForEach-Object {
     $Url = "https://www.nrc.nl$($_)"
     $DateText = (($Url -split '/')[4..6]) -join '-'
     $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
-    $Title = ($Content | pup 'h1[data-flowtype="headline"] text{}')
-    $Body = ((($Content | pup 'div.content p text{}') -join ' ') -replace $SmartSingleQuotes, '''') -replace $SmartDoubleQuotes, '"'
-    $Date = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
-    $Payload = (@{fields = @{ Url = $Url; Title = $Title; Body = $Body; Date = $Date }} | ConvertTo-Json -Depth 5)
+$Title = ($Content | pup 'h1[data-flowtype="headline"] text{}')
+$Body = ((($Content | pup 'div.content p text{}') -join ' ') -replace $SmartSingleQuotes, '''') -replace $SmartDoubleQuotes, '"'
+$Date = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
 
-    Write-Verbose $Payload
-
-    Invoke-RestMethod `
-        -Method POST `
-        -Uri 'https://api.airtable.com/v0/appB4Jzod47gLXUVE/ikjes' `
-        -ContentType 'application/json' `
-        -Headers @{'Authorization' = "Bearer $($env:AirTableApiKey)"} `
-        -Body $Payload
+if (-not (Test-AirTableRecord -TableName ikjes -FieldName Url -Value $Url))
+{
+    New-AirTableRecord `
+        -TableName ikjes `
+        -Fields @{ Url = $Url; Title = $Title; Body = $Body; Date = $Date }
+}
+else
+{
+    Write-Verbose "An AirTable record with Url $Url already exists; skipping."
+}
 }
