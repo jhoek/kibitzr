1f9cbc4e0674ff338428d0375f305595feee7590
Jan Hoek <github@uncommonsense.nl>
2019-06-13 16:49:30 +0200

    Updated luckytv script

--- luckytv/script.ps1
+++ luckytv/script.ps1
@@ -2,11 +2,11 @@ $ProgressPreference = 'SilentlyContinue'
 $DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
 
 Invoke-WebRequest -Uri 'http://www.luckytv.nl/afleveringen/' `
-    | Select-Object -ExpandProperty Content `
-    | pup --plain 'article.video div json{}' `
-    | ConvertFrom-Json -AsHashtable `
-    | ForEach-Object { $_.GetEnumerator() } `
-    | ForEach-Object {
+| Select-Object -ExpandProperty Content `
+| pup --plain 'article.video div json{}' `
+| ConvertFrom-Json -AsHashtable `
+| ForEach-Object { $_.GetEnumerator() } `
+| ForEach-Object {
     $Link = $_['children'][0]
     $MetaData = $_['children'][1]
 
@@ -17,13 +17,14 @@ Invoke-WebRequest -Uri 'http://www.luckytv.nl/afleveringen/' `
     $Url = $Link.href
     $ThumbNail = $Link['children'][0].src
 
-    $Payload = (@{fields = @{ Url = $Url; Title = $Title; Date = $Date; ThumbNail = $ThumbNail }} | ConvertTo-Json -Depth 5)
-    Write-Verbose $Payload
-
-    Invoke-RestMethod `
-        -Method POST `
-        -Uri 'https://api.airtable.com/v0/appB4Jzod47gLXUVE/luckytv' `
-        -ContentType 'application/json' `
-        -Headers @{'Authorization' = "Bearer $($env:AirTableApiKey)"} `
-        -Body $Payload
+    if (-not (Test-AirTableRecord -TableName luckytv -FieldName Url -Value $Url))
+    {
+        New-AirTableRecord `
+            -TableName luckytv `
+            -Fields @{ Url = $Url; Title = $Title; Date = $Date; ThumbNail = $ThumbNail }
+    }
+    else 
+    {
+        Write-Verbose "An AirTable record with Url $Url already exists; skipping."
+    }    
 }
