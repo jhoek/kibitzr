d766e7676ddaa3905ddd83b912bd24773bcdc817
Jan Hoek <github@uncommonsense.nl>
2019-06-16 15:20:41 +0200

    Added body and date

--- kraai/script.ps1
+++ kraai/script.ps1
@@ -1,14 +1,17 @@
 . ../Save-EntryToAirTable.ps1
 $ProgressPreference = 'SilentlyContinue'
+$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
 
 Invoke-WebRequest -Uri https://www.trouw.nl/de-kraai~d10778f08-8c81-490e-a656-93b2f29ac289 `
 | Select-Object -ExpandProperty Content `
 | pup 'article > a:first-of-type attr{href}' `
-| Select-Object -First 1 `
 | ForEach-Object {
     $Url = "https://trouw.nl{0}" -f $_
     $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
 $Title = $Content | pup 'h1 text{}'
-$Body = $Content | pup --plain 'p.article__paragraph text{}'
-Save-EntryToAirTable -TableName kraai -Url $Url -Title $Title -Body $Body
+$Body = ($Content | pup --plain 'p.article__paragraph text{}') -join "`n`n"
+$DateText = $Content | pup 'time text{}'
+$Date = [DateTime]::ParseExact($DateText, 'H:mm, d MMMM yyyy', $DutchCulture)
+
+Save-EntryToAirTable -TableName kraai -Url $Url -Title $Title -Body $Body -Date $Date
 }
--- sudoku/script.ps1
+++ sudoku/script.ps1
@@ -1,18 +1,18 @@
 while ($true)
 {
     $Content = Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' | Select-Object -ExpandProperty Content
-    $NoScript = ($Content | pup --plain '.nmt-layout .nmt-item:first-of-type noscript') -join ' '
-    $Link = [regex]::Match($NoScript, 'src="(.*?)"').Groups[1].Value
-    $Hyperlink = ($Content | pup '.nmt-layout .nmt-item:first-of-type a attr{href}')
-    $Date = (($HyperLink -split '/')[2..4]) -join '.'
+$NoScript = ($Content | pup --plain '.nmt-layout .nmt-item:first-of-type noscript') -join ' '
+$Link = [regex]::Match($NoScript, 'src="(.*?)"').Groups[1].Value
+$Hyperlink = ($Content | pup '.nmt-layout .nmt-item:first-of-type a attr{href}')
+$Date = (($HyperLink -split '/')[2..4]) -join '.'
 
-    Send-PushoverNotification `
-        -ApplicationToken av5b3yspt9nzrphgdpq135fa64do6j `
-        -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-        -Title Sudoku `
-        -Message $Date `
-        -SupplementaryUrl $Link `
-        -SupplementaryUrlTitle Link
+Send-PushoverNotification `
+    -ApplicationToken av5b3yspt9nzrphgdpq135fa64do6j `
+    -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+    -Title Sudoku `
+    -Message $Date `
+    -SupplementaryUrl $Link `
+    -SupplementaryUrlTitle Link
 
-    Start-Sleep -Seconds (60 * 60 * 24)
+Start-Sleep -Seconds (60 * 60 * 24)
 }
