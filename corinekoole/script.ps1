77533db89a46f15ea1a1a045d2f849219c31d39e
Jan Hoek <github@uncommonsense.nl>
2019-06-16 14:52:30 +0200

    Added date and body

--- corinekoole/script.ps1
+++ corinekoole/script.ps1
@@ -1,5 +1,6 @@
 . ../Save-EntryToAirTable.ps1
 $ProgressPreference = 'SilentlyContinue'
+$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
 
 Invoke-WebRequest -Uri https://www.volkskrant.nl/auteur/corine-koole `
 | Select-Object -ExpandProperty Content `
@@ -7,9 +8,10 @@ Invoke-WebRequest -Uri https://www.volkskrant.nl/auteur/corine-koole `
 | ForEach-Object {
     $Url = "https://volkskrant.nl$($_)"
     $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
-
 $Title = $Content | pup 'h1 text{}'
-$Body = $Content | pup 'section.artstyle__main--container p' | ForEach-Object { $_; '' }
+$Body = ($Content | pup 'section.artstyle__main--container p text{}') -join "`n`n"
+$DateText = ($Content | pup 'time span text{}') -join ''
+$Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy, HH:mm', $DutchCulture)
 
-Save-EntryToAirTable -TableName corinekoole -Url $Url -Title $Title
+Save-EntryToAirTable -TableName corinekoole -Url $Url -Title $Title -Date $Date -Body $Body
 }
