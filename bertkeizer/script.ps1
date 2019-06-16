89898fd90e234574c4bc7a98e626ebebf7182aa4
Jan Hoek <github@uncommonsense.nl>
2019-06-16 14:18:21 +0200

    Include dates

--- bertkeizer/script.ps1
+++ bertkeizer/script.ps1
@@ -1,5 +1,6 @@
 . ../Save-EntryToAirTable.ps1
 $ProgressPreference = 'SilentlyContinue'
+$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
 
 Invoke-WebRequest -Uri https://trouw.nl/bert-keizer~d5840135188caea36a9581181 `
 | Select-Object -ExpandProperty Content `
@@ -9,6 +10,8 @@ Invoke-WebRequest -Uri https://trouw.nl/bert-keizer~d5840135188caea36a9581181 `
     $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
 $Title = $Content | pup 'h1 text{}'
 $Body = $Content | pup --plain 'p.article__paragraph text{}'
+$DateText = $Content | pup 'time text{}'
+$Date = [DateTime]::ParseExact($DateText, 'HH:mm, d MMMM yyyy', $DutchCulture)
 
-Save-EntryToAirTable -TableName bertkeizer -Url $Url -Title $Title -Body $Body
+Save-EntryToAirTable -TableName bertkeizer -Url $Url -Title $Title -Body $Body -Date $Date
 }
