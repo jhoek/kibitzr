4eba88842be0d562963936290a0f1acc8555d09c
Jan Hoek <github@uncommonsense.nl>
2019-06-24 15:31:08 +0200

    More work on script

--- sudoku/script.ps1
+++ sudoku/script.ps1
@@ -7,22 +7,12 @@ Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' `
 | ConvertFrom-Json -Depth 10 `
 | ForEach-Object { $_.GetEnumerator() } `
 | ForEach-Object {
-    # $Link = [regex]::Match($NoScript, 'src="(.*?)"').Groups[1].Value
-    ($_.children[0].children[0].children[1].text) | Select-String -Pattern 'src="(.*?)"' | Select-Object -ExpandProperty Matches | Select-Object -Skip 1 -First 1 -ExpandProperty Value
+    $null = ($_.children[0].children[0].children[1].text) -match 'src="(.*?)"'
 
-    $Url = $_.ref
-    $DateElements = $Url -split '/'
+    $Body = '<img src="{0}"></img>' -f $Matches[1]
+    $Url = 'https://www.nrc.nl{0}' -f $_.href
+    $DateElements = ($_.href) -split '/'
     $Date = Get-Date -Year $DateElements[2] -Month $DateElements[3] -Day $DateElements[4]
-    #$Body = '<img src="{0}"></img>' -f 
 
     Save-EntryToAirTable -TableName sudoku -Url $Url -Date $Date -Title Sudoku -Body $Body
 }
-
-
-
-# $Hyperlink = ($Content | pup '.nmt-layout .nmt-item:first-of-type a attr{href}')
-
-# Send-PushoverNotification `
-#     -Message $Date `
-#     -SupplementaryUrl $Link `
-#     -SupplementaryUrlTitle Link
