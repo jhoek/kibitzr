47c09aba37b26dc389a2edb3bc8479fa181f6a0e
Jan Hoek <github@uncommonsense.nl>
2019-06-24 15:18:26 +0200

    Interim check-in

--- sudoku/script.ps1
+++ sudoku/script.ps1
@@ -1,18 +1,28 @@
-while ($true)
-{
-    $Content = Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' | Select-Object -ExpandProperty Content
-$NoScript = ($Content | pup --plain '.nmt-layout .nmt-item:first-of-type noscript') -join ' '
-$Link = [regex]::Match($NoScript, 'src="(.*?)"').Groups[1].Value
-$Hyperlink = ($Content | pup '.nmt-layout .nmt-item:first-of-type a attr{href}')
-$Date = (($HyperLink -split '/')[2..4]) -join '.'
+. ../Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
 
-Send-PushoverNotification `
-    -ApplicationToken av5b3yspt9nzrphgdpq135fa64do6j `
-    -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-    -Title Sudoku `
-    -Message $Date `
-    -SupplementaryUrl $Link `
-    -SupplementaryUrlTitle Link
+Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' `
+| Select-Object -ExpandProperty Content `
+| pup --plain '.nmt-item__link json{}' `
+| ConvertFrom-Json -Depth 10 `
+| ForEach-Object { $_.GetEnumerator() } `
+| ForEach-Object {
+    # $Link = [regex]::Match($NoScript, 'src="(.*?)"').Groups[1].Value
+    ($_.children[0].children[0].children[1].text) | Select-String -Pattern 'src="(.*?)"' | Select-Object -ExpandProperty Matches | Select-Object -Skip 1 -First 1 -ExpandProperty Value
 
-Start-Sleep -Seconds (60 * 60 * 24)
+    $Url = $_.ref
+    $DateElements = $Url -split '/'
+    $Date = Get-Date -Year $DateElements[2] -Month $DateElements[3] -Day $DateElements[4]
+    #$Body = '<img src="{0}"></img>' -f 
+
+    Save-EntryToAirTable -TableName sudoku -Url $Url -Date $Date -Title Sudoku -Body $Body
 }
+
+
+
+# $Hyperlink = ($Content | pup '.nmt-layout .nmt-item:first-of-type a attr{href}')
+
+# Send-PushoverNotification `
+#     -Message $Date `
+#     -SupplementaryUrl $Link `
+#     -SupplementaryUrlTitle Link
