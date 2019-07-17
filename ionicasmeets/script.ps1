83081beccd05f0300e096a9a9640a57a0f6e716f
Jan Hoek <github@uncommonsense.nl>
2019-07-17 17:47:00 +0200

    Added Ionica Smeets

---
+++ ionicasmeets/script.ps1
@@ -0,0 +1,17 @@
+. ../Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
+
+Invoke-WebRequest -Uri https://www.volkskrant.nl/auteur/Ionica%20Smeets `
+| Select-Object -ExpandProperty Content `
+| pup 'article a attr{href}' `
+| ForEach-Object {
+    $Url = "https://volkskrant.nl$($_)"
+    $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
+$Title = $Content | pup 'h1 text{}' --plain
+$Body = $Content | pup 'section.artstyle__main--container p text{}' --plain | ForEach-Object { $_; '' }
+$DateText = $Content | pup 'time span:first-child text{}'
+$Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy', $DutchCulture)
+
+Save-EntryToAirTable -TableName ionicasmeets -Url $Url -Date $Date -Title $Title -Body $Body
+}
