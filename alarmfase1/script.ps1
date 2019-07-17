a24a00ea2391738b8c8d14b955a52241b559474c
Jan Hoek <github@uncommonsense.nl>
2019-07-17 14:59:44 +0200

    Only high priority entries

--- alarmfase1/script.ps1
+++ alarmfase1/script.ps1
@@ -4,6 +4,7 @@ $ProgressPreference = 'SilentlyContinue'
 'amsterdam-amstelland', 'gooi-en-vechtstreek', 'zuid-holland-zuid' `
 | ForEach-Object {
     Get-P2000Entry -Region $_ `
+    | Where-Object Priority -in 'P1', 'A1' `
     | ForEach-Object {
         Save-EntryToAirTable `
             -TableName alarmfase1 `
