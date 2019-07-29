490d9f1d112b8051d7b1e85e762a54aed02d6701
Jan Hoek <github@uncommonsense.nl>
2019-07-29 13:08:14 +0200

    Updated after site update

--- sudoku/script.ps1
+++ sudoku/script.ps1
@@ -3,17 +3,14 @@ $ProgressPreference = 'SilentlyContinue'
 
 Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/sudoku/' `
 | Select-Object -ExpandProperty Content `
-| pup --plain '.nmt-item__link json{}' `
-| ConvertFrom-Json -Depth 10 `
-| ForEach-Object { $_.GetEnumerator() } `
+| pup '.compact-grid__item a attr{href}' --plain `
+| Select-Object -Skip 1 -First 10 `
 | ForEach-Object {
-    $null = ($_.children[0].children[0].children[1].text) -match 'src="(.*?)"'
-
-    $Body = 'https://www.nrc.nl{0}' -f $_.href
-    $Url = $Matches[1]
-    $DateElements = ($_.href) -split '/'
+    $Url = "https://www.nrc.nl$($_)"
+    $DateElements = $_ -split '/'
     $Date = Get-Date -Year $DateElements[2] -Month $DateElements[3] -Day $DateElements[4]
+    $Image = (Invoke-WebRequest -Uri $Url).Images.src
     $Title = 'Sudoku {0:dddd d MMMM yyyy}' -f $Date
 
-    Save-EntryToAirTable -TableName sudoku -Url $Url -Date $Date -Title $Title -Body $Body
+    Save-EntryToAirTable -TableName sudoku -Url $Image -Date $Date -Title $Title -Body $Url
 }
