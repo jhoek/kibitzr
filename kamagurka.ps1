8bd32931f6dc9ff8558a6810c8c0a14feb7e12ee
Jan Hoek <github@uncommonsense.nl>
2019-08-09 13:09:55 +0200

    Added new source

---
+++ kamagurka.ps1
@@ -0,0 +1,18 @@
+#!/usr/bin/pwsh
+. $PSScriptRoot/Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+
+Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/kamagurka/' `
+| Select-Object -ExpandProperty Content `
+| pup '.nmt-item a json{}' --plain `
+| ConvertFrom-Json -Depth 10 -AsHashtable `
+| ForEach-Object { $_.GetEnumerator() } `
+| ForEach-Object {
+    $Url = [regex]::Match($_.children[0].children[0].children[1].text, 'src="(.*)"').Groups[1].Value
+    $Body = "https://www.nrc.nl$($_.href)"
+    $DateElements = $_.href -split '/'
+    $Date = Get-Date -Year $DateElements[2] -Month $DateElements[3] -Day $DateElements[4]
+    $Title = 'Kamagurka {0:dddd d MMMM yyyy}' -f $Date
+
+    Save-EntryToAirTable -TableName kamagurka -Url $Url -Date $Date -Title $Title -Body $Body
+}
