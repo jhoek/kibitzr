5bb58085c60357937bcfe2fda83252c4d6c66d31
Jan Hoek <github@uncommonsense.nl>
2019-08-12 11:56:45 +0200

    Added source

---
+++ zomeravondgesprek.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/pwsh
+. $PSScriptRoot/Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+
+Invoke-WebRequest -Uri https://www.nrc.nl/dossier/zomeravondgesprek/ `
+| Select-Object -ExpandProperty Content `
+| pup '.nmt-item a attr{href}' --plain `
+| ForEach-Object {
+    $Url = "https://www.nrc.nl$($_)"
+    $DateText = (($Url -split '/')[4..6]) -join '-'
+    $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
+$Title = ($Content | pup 'h1[data-flowtype="headline"] text{}' --plain)
+$Body = ($Content | pup 'div.content p text{}' --plain) -join ' '
+$Date = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
+
+Save-EntryToAirTable -TableName zomeravondgesprek -Url $Url -Date $Date -Title $Title -Body $Body
+}
