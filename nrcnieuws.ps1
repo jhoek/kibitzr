20c1aa326cc4ef7cac5741767778a57e13fc986a
Jan Hoek <github@uncommonsense.nl>
2019-08-29 15:29:39 +0200

    Added nrcnieuws

--- nrcnieuws.ps1
+++ nrcnieuws.ps1
@@ -1,3 +1,6 @@
+. $PSScriptRoot/Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+
 Invoke-WebRequest -Uri https://www.nrc.nl/nieuws/ `
 | Select-Object -ExpandProperty Content `
 | pup '.nmt-item__inner json{}' --plain `
@@ -5,8 +8,13 @@ Invoke-WebRequest -Uri https://www.nrc.nl/nieuws/ `
 | ForEach-Object { $_.GetEnumerator() } `
 | Select-Object -ExpandProperty children `
 | ForEach-Object {
-    $Link = $_.$href
-    $Title = "$($_.children[1].children[0].children[0].text): $($_.children[1].children[1].text)"
-    $Body = $_.children[1].children[2].children[0].text
-    $Body
+    $Url = "https://www.nrc.nl$($_.href)"
+    $SubTitle = $_.children[1].children[0].children[0].text
+    $MainTitle = $_.children[1].children[1].text
+    $Title = "$SubTitle - $MainTitle"
+    $Body = $_.children[1].children[2].text
+    $DateElements = ($_.href -split '/')[2..4]
+    $Date = Get-Date -Year $DateElements[0] -Month $DateElements[1] -Day $DateElements[2]
+
+    Save-EntryToAirTable -TableName nrcnieuws -Url $Url -Date $Date -Title $Title -Body $Body    
 }
