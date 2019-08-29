ff30341c104e30692098145190fc46d0942085b8
Jan Hoek <github@uncommonsense.nl>
2019-08-29 16:33:12 +0200

    Use jq

--- nrcnieuws.ps1
+++ nrcnieuws.ps1
@@ -4,17 +4,14 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://www.nrc.nl/nieuws/ `
 | Select-Object -ExpandProperty Content `
 | pup '.nmt-item__inner json{}' --plain `
+| jq '[.[] | { url:.children[0].href, subtitle:.children[0].children[1].children[0].children[0].text, maintitle:.children[0].children[1].children[1].text, body:.children[0].children[1].children[2].text, body1:.children[0].children[1].children[2].children[0].text }]' `
 | ConvertFrom-Json `
 | ForEach-Object { $_.GetEnumerator() } `
-| Select-Object -ExpandProperty children `
 | ForEach-Object {
-    $Url = "https://www.nrc.nl$($_.href)"
-    $SubTitle = $_.children[1].children[0].children[0].text
-    $MainTitle = $_.children[1].children[1].text
-    $Title = "$SubTitle - $MainTitle"
-    $Body = $_.children[1].children[2].text
-    $DateElements = ($_.href -split '/')[2..4]
+    $Url = "https://www.nrc.nl$($_.url)"
+    $Title = "$($_.SubTitle) - $($_.MainTitle)"
+    $Body = $_.body + $_.body1
+    $DateElements = ($_.url -split '/')[2..4]
     $Date = Get-Date -Year $DateElements[0] -Month $DateElements[1] -Day $DateElements[2]
-
     Save-EntryToAirTable -TableName nrcnieuws -Url $Url -Date $Date -Title $Title -Body $Body    
 }
