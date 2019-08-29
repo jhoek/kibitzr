a254a42a6a87909217200f6220bf8c810b8ce27b
Jan Hoek <github@uncommonsense.nl>
2019-08-29 12:36:10 +0200

    More work on nrc nieuws

--- nrcnieuws.ps1
+++ nrcnieuws.ps1
@@ -6,7 +6,7 @@ Invoke-WebRequest -Uri https://www.nrc.nl/nieuws/ `
 | Select-Object -ExpandProperty children `
 | ForEach-Object {
     $Link = $_.$href
-    $_ `
-    | Select-Object -ExpandProperty children `
-    | gm
+    $Title = "$($_.children[1].children[0].children[0].text): $($_.children[1].children[1].text)"
+    $Body = $_.children[1].children[2].children[0].text
+    $Body
 }
