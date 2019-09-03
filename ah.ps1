2c9a9837da1a2a7a2e3a0f95829dcf851df7e7cc
Jan Hoek <github@uncommonsense.nl>
2019-09-03 13:10:46 +0200

    Simplified ah table

--- ah.ps1
+++ ah.ps1
@@ -1,12 +1,21 @@
+Find-AirTableRecord -TableName ah `
+| ForEach-Object { Remove-AirTableRecord -TableName ah -RecordID $_.AirTableRecordID }
+
 Get-AHStore -ID 1844, 1083, 1541, 1855 `
 | ForEach-Object {
-    $Description = "$($_.City), $($_.Street)"
+    $Store = "$($_.City), $($_.Street)"
+
     $_.OpeningHours `
     | ForEach-Object {
+        $Date = $_.From.Date
+        $From = $_.From.ToString('HH:mm')
+        $To = $_.To.ToString('HH:mm')
+        $OpeningHours = "$From..$To"
+
         $Fields = @{
-            FromTime    = $_.From.ToUniversalTime()
-            ToTime      = $_.To.ToUniversalTime()
-            Description = $Description
+            Store        = $Store
+            Date         = $Date
+            OpeningHours = $OpeningHours
         }
 
         New-AirTableRecord `
