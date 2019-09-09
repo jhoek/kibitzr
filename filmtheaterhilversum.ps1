8086ce6fed96a3987ca5a857f435a46f56ec03ae
Jan Hoek <github@uncommonsense.nl>
2019-09-09 21:22:50 +0200

    Added filmtheaterhilversum

---
+++ filmtheaterhilversum.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/pwsh
+. $PSScriptRoot/Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+
+Invoke-WebRequest -Uri https://www.filmtheaterhilversum.nl/films/ `
+| Select-Object -ExpandProperty Content `
+| pup '.film-index__film attr{href}' --plain `
+| ForEach-Object {
+    $Url = "https://www.filmtheaterhilversum.nl$($_)"
+    $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
+    $Title = ($Content | pup 'h1.featured__title text{}' --plain)
+    $Body = ($Content | pup '.film__synopsis__intro text{}' --plain) -join ' '
+    $Date = Get-Date
+
+    Save-EntryToAirTable -TableName filmtheaterhilversum -Url $Url -Date $Date -Title $Title -Body $Body
+}
