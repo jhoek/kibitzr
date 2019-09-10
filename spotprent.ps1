420b40b756d285f553a0ab4b29f0a2d9a8fcb99e
Jan Hoek <github@uncommonsense.nl>
2019-09-10 16:34:38 +0200

    Added spotprent

--- spotprent.ps1
+++ spotprent.ps1
@@ -1,28 +1,28 @@
 #!/usr/bin/pwsh
 . $PSScriptRoot/Save-EntryToAirTable.ps1
 $ProgressPreference = 'SilentlyContinue'
-$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
 
-$Elements = Invoke-WebRequest -Uri https://www.trouw.nl/cartoons/spotprent-van-de-dag~b1551c3b/ `
+Invoke-WebRequest -Uri https://www.trouw.nl/cartoons/spotprent-van-de-dag~b1551c3b/ `
 | Select-Object -ExpandProperty Content `
-| pup 'article section.artstyle__main--container json{}' --plain `
-| ConvertFrom-Json -Depth 10 `
-| ForEach-Object { $_.children } `
-| Where-Object tag -in 'h3', 'figure'
-
-while ($Elements)
-{
-    $First, $Second, $Elements = $Elements
-    $First
-    $Second
-    $DateText = $First.Text
-    $Date = $Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy', $DutchCulture)
-    $Title = "Spotprent van de dag $DateText"
-    $Url = $Second.children.'data-original'
-
-    # $Title
-    # $url
-    # $Date
-
-    #Save-EntryToAirTable -TableName antondingeman -Url $Url -Title $Title -Body $Url -Date $Date
+| pup '.artstyle__image attr{data-original}' --plain `
+| Select-Object -First 10 `
+| ForEach-Object {
+    Save-EntryToAirTable -TableName spotprent -Url $_ -Title ('Spotprent {0:dddd d MMMM yyyy}' -f (Get-Date)) -Body $_ -Date (Get-Date)
 }
+
+# while ($Elements)
+# {
+#     $First, $Second, $Elements = $Elements
+#     $First.Text
+#     $Second.Text
+#     $DateText = $First.Text
+#     $Date = $Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy', $DutchCulture)
+#     $Title = "Spotprent van de dag $DateText"
+#     $Url = $Second.children.'data-original'
+
+#     # $Title
+#     # $url
+#     # $Date
+
+#     #
+# }
