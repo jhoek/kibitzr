1ad2d82515b9d35992b21569f2bc05762f99f138
Jan Hoek <github@uncommonsense.nl>
2019-09-29 18:41:21 +0200

    More work

---
+++ Send-KibitzrNotification.ps1
@@ -0,0 +1,54 @@
+function Send-KibitzrNotification
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]$Url,
+
+        [Parameter(Mandatory)]
+        [string]$ApplicationToken,
+
+        [Parameter(Mandatory)]
+        [string]$Recipient,
+
+        [Parameter(Mandatory)]
+        [string]$Message,
+
+        [string]$Title,
+
+        [string]$ImageUrl
+    )
+
+    switch (Test-AirTableRecord -BaseName kibitzr -TableName notifications -FieldName Url -Value $Url)
+    {
+        $false
+        {
+            $Parameters = @{ }
+            $Parameters.ApplicationToken = $ApplicationToken
+            $Parameters.Recipient = $Recipient
+            $Parameters.Title = $Title
+            $Parameters.Message = $Message
+
+            if ($ImageUrl)
+            {
+                $TempFilePath = New-TemporaryFile
+                Invoke-WebRequest -Uri $ImageUrl -OutFile $TempFilePath
+                $Parameters.AttachmentFileName = $TempFilePath
+            }
+
+            Send-PushoverNotification @Parameters
+
+            if ($ImageUrl)
+            {
+                Remove-Item -Path $TempFilePath -ErrorAction SilentlyContinue
+            }
+
+            New-AirTableRecord -BaseName kibitzr -TableName notifications -Fields @{Url = $Url }
+        }
+
+        $true
+        {
+            Write-Warning "Notification for $Url already sent."
+        }
+    }
+}
--- antondingeman.ps1
+++ antondingeman.ps1
@@ -1,30 +1,14 @@
 #!/usr/bin/pwsh
-. $PSScriptRoot/Save-EntryToAirTable.ps1
-$ProgressPreference = 'SilentlyContinue'
-$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
+. $PSScriptRoot/Send-KibitzrNotification.ps1
 
-$Elements = Invoke-WebRequest -Uri https://www.trouw.nl/cartoons/de-wereld-van-anton-dingeman~b21b94dc/ `
+Invoke-WebRequest -Uri https://www.trouw.nl/cartoons/de-wereld-van-anton-dingeman~b21b94dc/ `
 | Select-Object -ExpandProperty Content `
-| pup 'article section.artstyle__main--container json{}' --plain `
-| ConvertFrom-Json -Depth 10 `
-| ForEach-Object { $_.children } `
-| Select-Object -First 20
-
-while ($Elements)
-{
-    $First, $Second, $Elements = $Elements
-    $DateText = $First.Text
-    $Date = $Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy', $DutchCulture)
-    $Title = "Anton Dingeman $DateText"
-    $Url = $Second.children.'data-original'
-
-    if (-not (Test-NotificationAlreadySent -Url $Url))
-    {
-        Send-PushoverNotification `
-            -ApplicationToken aRkTUg5jtr9pSDQBPYwPN9X5dP2mHB `
-            -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-            -AttachmentFileName
-    }
-
-    Save-EntryToAirTable -TableName notifications -Url $Url -Title $Title -Body $Url -Date $Date
+| pup 'img.artstyle__image attr{data-original}' --plain `
+| ForEach-Object {
+    Send-KibitzrNotification `
+        -Url $Url `
+        -ApplicationToken aRkTUg5jtr9pSDQBPYwPN9X5dP2mHB `
+        -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+        -Message 'Anton Dingeman' `
+        -ImageUrl $_
 }
