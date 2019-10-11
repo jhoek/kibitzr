60d4f3e7e48e9b2cadfe5f4b97ff0ef5e24378b8
Jan Hoek <github@uncommonsense.nl>
2019-10-11 12:11:15 +0200

    Added title prefix

--- taal.ps1
+++ taal.ps1
@@ -14,5 +14,5 @@ Invoke-WebRequest -Uri https://www.trouw.nl/dossier/taal `
     $DateText = $Content | pup 'time text{}' --plain
     $Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy , H:mm', $DutchCulture)
 
-    Save-EntryToAirTable -Url $Url -Title $Title -Body $Body -Date $Date -Verbose
+    Save-EntryToAirTable -Url $Url -Title "Taal: $Title" -Body $Body -Date $Date -Verbose
 }
