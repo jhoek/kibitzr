fae1ac8b35582d174da0dc77d601fbb1e8056f18
Jan Hoek <github@uncommonsense.nl>
2019-11-09 14:50:46 +0100

    Removed comments; added shebang

--- showinfo.ps1
+++ showinfo.ps1
@@ -1,22 +1,4 @@
-# $GuidHistoryPath = "~/rssguids.txt"
-# $GuidHistory = switch (Test-Path -path $GuidHistoryPath)
-# {
-#     $true { Get-Content -Path $GuidHistoryPath }
-#     $false { @() }
-# }
-# $NewHistory = New-Object -TypeName System.Collections.ArrayList
-
-# Get-RssFeedItem -Uri 'http://showrss.info/user/214003.rss?magnets=true&namespaces=true&name=null&quality=null&re=null' `
-# | Where-Object Guid -notin $GuidHistory `
-# | Where-Object Guid -notin $NewHistory `
-# | ForEach-Object {
-
-
-#     $NewHistory.Add($_.Guid)
-# }
-
-# Add-Content -Path $GuidHistoryPath -Value $NewHistory
-
+#!/usr/bin/pwsh
 . ./Send-KibitzrNotification.ps1
 
 Get-RssFeedItem -Uri 'http://showrss.info/user/214003.rss?magnets=true&namespaces=true&name=null&quality=null&re=null' `
