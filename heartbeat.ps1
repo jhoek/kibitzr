6e87834b2eb5f44e63da7de23a3ed04e2b17e1c9
Jan Hoek <github@uncommonsense.nl>
2020-02-05 20:40:16 +0100

    Added heartbeat

---
+++ heartbeat.ps1
@@ -0,0 +1,6 @@
+#!/usr/bin/pwsh
+Send-PushoverNotification `
+    -ApplicationToken a7uyacvxzcq71ywyjdn95g6aoxnj8p `
+    -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+    -Message 'Raspberry Pi is still running' `
+    -Priority Lowest
