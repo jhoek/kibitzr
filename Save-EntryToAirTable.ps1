6623882c0f22979b508f7953f4f96318d59f3e34
Jan Hoek <github@uncommonsense.nl>
2020-06-28 21:44:52 +0200

    Allow diffent target tables

--- Save-EntryToAirTable.ps1
+++ Save-EntryToAirTable.ps1
@@ -5,22 +5,23 @@ function Save-EntryToAirTable
         [string]$Url,
         [DateTime]$Date,
         [string]$Title,
-        [string]$Body
+        [string]$Body,
+        [string]$TableName = 'rss'
     )
 
     $SmartSingleQuotes = '[\u2019\u2018\u201A]'
     $SmartDoubleQuotes = '[\u201C\u201D\u201E]'
 
-    if (-not (Test-AirTableRecord -TableName rss -FieldName Url -Value $Url))
+    if (-not (Test-AirTableRecord -TableName $TableName -FieldName Url -Value $Url))
     {
         $Body = ($Body -replace $SmartSingleQuotes, '''') -replace $SmartDoubleQuotes, '"'
 
         New-AirTableRecord `
-            -TableName rss `
+            -TableName $TableName `
             -Fields @{ Url = $Url; Title = $Title; Body = $Body; Date = $Date }
     }
     else
     {
-        Write-Warning "Entry for '$Url' already exists in table 'rss'."
+        Write-Warning "Entry for '$Url' already exists in table '$TableName'."
     }
 }
