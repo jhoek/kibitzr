a23d11572a57eeec3d5b9b2671c72947a6a19374
Jan Hoek <github@uncommonsense.nl>
2020-07-07 09:39:26 +0200

    More to mail

--- sylviawitteman.ps1
+++ sylviawitteman.ps1
@@ -14,5 +14,5 @@ Invoke-WebRequest -Uri https://www.volkskrant.nl/auteur/Sylvia%20Witteman `
     $DateText = $Content | pup 'time span:first-child text{}'
     $Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy', $DutchCulture)
 
-    Save-EntryToAirTable -Url $Url -Date $Date -Title "Sylvia Witteman: $Title" -Body $Body
+    Save-EntryToAirTable -Url $Url -Date $Date -Title "Sylvia Witteman: $Title" -Body $Body -TableName Mail
 }
