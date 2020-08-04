73a0c5d82c8fc76575b70e0ab15ce83fafd2d69c
Jan Hoek <github@uncommonsense.nl>
2020-08-04 09:38:35 +0200

    Prevent a deluge of notifications around midnight

--- teletekst.ps1
+++ teletekst.ps1
@@ -6,7 +6,7 @@ Get-TeletekstNews -Type Domestic, Foreign `
 | ForEach-Object {
     Send-KibitzrNotification `
         -Url $_.Link `
-        -UniqueID "$(Get-Date -Date $_.DateTime -Format 'yyyyMMdd')$($_.Title -replace '''', '')" `
+        -UniqueID "$($_.Title) - $($_.Content)".GetHashCode() `
         -ApplicationToken asxmmq8g95jt4ed1qcrucdvu2iuy67 `
         -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
         -Title $_.Title `
