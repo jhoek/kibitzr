4df703f02998ed4cb5b38c9a579fccbb447f29f3
Jan Hoek <github@uncommonsense.nl>
2020-08-05 09:44:42 +0200

    Use Base64 instead of hashing, since the latter is not guaranteed to be stable between application domains

--- teletekst.ps1
+++ teletekst.ps1
@@ -2,11 +2,24 @@
 . $PSScriptRoot/Send-KibitzrNotification.ps1
 Import-Module UncommonSense.Teletekst
 
+function ConvertTo-Base64
+{
+    param
+    (
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Value
+    )
+
+    [System.Convert]::ToBase64String(
+        [System.Text.Encoding]::Unicode.GetBytes($Value)
+    )
+}
+
 Get-TeletekstNews -Type Domestic, Foreign `
 | ForEach-Object {
     Send-KibitzrNotification `
         -Url $_.Link `
-        -UniqueID "$($_.Title) - $($_.Content)".GetHashCode() `
+        -UniqueID (ConvertTo-Base64 -Value "$($_.Title) - $($_.Content)") `
         -ApplicationToken asxmmq8g95jt4ed1qcrucdvu2iuy67 `
         -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
         -Title $_.Title `
