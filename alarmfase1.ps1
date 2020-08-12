c3434360d0a78a044265b47b8b76ab0ef9bca9a1
Jan Hoek <github@uncommonsense.nl>
2020-08-12 09:18:03 +0200

    Removed Amstelveen postal codes

--- alarmfase1.ps1
+++ alarmfase1.ps1
@@ -2,18 +2,18 @@
 $ProgressPreference = 'SilentlyContinue'
 $env:PATH = ($env:PATH, '/usr/local/bin') -join ':'
 
-1109, 3286, 1432, 1187, 1185 `
+1109, 3286, 1432 `
 | ForEach-Object {
-    Get-P2000Entry -Postcode $_ `
-    | Where-Object Priority -in 'P1', 'A1' `
-    | Where-Object DateTime -gt (Get-Date).AddMinutes(-10) `
+    Get-P2000Entry -PostCode $_ `
+    | Where-Object Priority -In 'P1', 'A1' `
+    | Where-Object DateTime -GT (Get-Date).AddMinutes(-10) `
     | Select-Object -First 10 `
     | ForEach-Object {
         Send-PushoverNotification `
-        -ApplicationToken aoiqq9zfsf32aodh55zoyg9rc3zhrh `
-        -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-        -Title $_.Title `
-        -Message $_.Original `
-        -SupplementaryUrl $_.Link
+            -ApplicationToken aoiqq9zfsf32aodh55zoyg9rc3zhrh `
+            -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+            -Title $_.Title `
+            -Message $_.Original `
+            -SupplementaryUrl $_.Link
     }
 }
