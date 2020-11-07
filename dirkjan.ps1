a358e6518cc6bb2cf3b07f69d6b5905896ac06a2
Jan Hoek <github@uncommonsense.nl>
2020-11-07 19:23:17 +0100

    Dirkjan

---
+++ dirkjan.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/pwsh
+
+. $PSScriptRoot/Send-KibitzrNotification.ps1
+
+Invoke-WebRequest -Uri https://www.parool.nl/kunst-media/dirkjan~bf34d4e2/ 
+| Select-Object -ExpandProperty Content
+| pup 'figure json{}' --plain
+| jq '[ .[].children[0].children[2] | { date: .\"data-title\", url: .\"data-original\" } ]'
+| ConvertFrom-Json
+| ForEach-Object {
+    Send-KibitzrNotification `
+        -Url $_.url `
+        -ApplicationToken arb2a6e1c5eomx796jbhkfmijeqrbq `
+        -Recipient gnkfssw1q1qp4pst9x3iu4rkrfzqi9 `
+        -Message "$($_.date)" `
+        -ImageUrl $_.url
+}
