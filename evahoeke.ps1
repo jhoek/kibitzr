e4ed3bfb4a63275e0198c66142e653a44f13e97d
Jan Hoek <github@uncommonsense.nl>
2020-11-07 12:30:30 +0100

    Script for Eva Hoeke's columns

---
+++ evahoeke.ps1
@@ -0,0 +1,18 @@
+#!/usr/bin/pwsh
+. $PSScriptRoot/Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
+
+Invoke-WebRequest -Uri https://www.volkskrant.nl/auteur/eva-hoeke `
+| Select-Object -ExpandProperty Content `
+| pup 'article a attr{href}' `
+| ForEach-Object {
+    $Url = "https://volkskrant.nl$($_)"
+    $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
+    $Title = $Content | pup 'h1 text{}' --plain
+    $Body = $Content | pup 'section.artstyle__main--container p text{}' --plain | ForEach-Object { $_; '' }
+    $DateText = $Content | pup 'time span:first-child text{}'
+    $Date = [DateTime]::ParseExact($DateText, 'd MMMM yyyy', $DutchCulture)
+
+    Save-EntryToAirTable -Url $Url -Date $Date -Title "Eva Hoeke: $Title" -Body $Body
+}
