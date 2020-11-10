13c2d2bddbe79cd7e03f6e90c4c83e6fe4e6ba84
Jan Hoek <github@uncommonsense.nl>
2020-11-10 10:05:38 +0100

    Process block should loop

--- joostprinsen2.ps1
+++ joostprinsen2.ps1
@@ -12,24 +12,26 @@ begin
 
 process
 {
-    Invoke-WebRequest $_
-    | Select-Object -ExpandProperty Content
-    | pup 'script json{}'
-    | ConvertFrom-Json
-    | Where-Object text -Like 'window`["__PRE*'
-    | Select-Object -First 1 -ExpandProperty Text
-    | ForEach-Object { $_ -replace '^window\["__PRELOADED_STATE_GRAPH__[^"]*"\] = ', '' }
-    | jq '[.[]][0] | { title: .title, intro: ([.intro.json[].p]|join(\" \")), body: ([.body.json[].p]|join(\" \"))}'
-    | ConvertFrom-Json
-    | ForEach-Object {
-        $Url -match 'DMF(\d{8})' | Out-Null
-        $Date = [DateTime]::ParseExact($Matches[1], 'yyyyMMdd', $DutchCulture)
+    $Url.ForEach{
+        Invoke-WebRequest $_
+        | Select-Object -ExpandProperty Content
+        | pup 'script json{}'
+        | ConvertFrom-Json
+        | Where-Object text -Like 'window`["__PRE*'
+        | Select-Object -First 1 -ExpandProperty Text
+        | ForEach-Object { $_ -replace '^window\["__PRELOADED_STATE_GRAPH__[^"]*"\] = ', '' }
+        | jq '[.[]][0] | { title: .title, intro: ([.intro.json[].p]|join(\" \")), body: ([.body.json[].p]|join(\" \"))}'
+        | ConvertFrom-Json
+        | ForEach-Object {
+            $Url -match 'DMF(\d{8})' | Out-Null
+            $Date = [DateTime]::ParseExact($Matches[1], 'yyyyMMdd', $DutchCulture)
 
-        Save-EntryToAirTable `
-            -Url $Url `
-            -Date $Date `
-            -Title $_.Title `
-            -Body @($_.Intro, $_.Body) -join ' ' `
-            -TableName mail
-    }
+            Save-EntryToAirTable `
+                -Url $Url `
+                -Date $Date `
+                -Title $_.Title `
+                -Body @($_.Intro, $_.Body) -join ' ' `
+                -TableName mail
+        }
+}
 }
