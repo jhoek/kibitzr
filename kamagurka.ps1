c556f5ec159b41b9fd4b9d83ce63911fc8f35937
Jan Hoek <github@uncommonsense.nl>
2020-11-16 18:20:12 +0100

    Repaired kamagurka

--- kamagurka.ps1
+++ kamagurka.ps1
@@ -4,21 +4,19 @@ $ProgressPreference = 'SilentlyContinue'
 
 Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/kamagurka/' `
 | Select-Object -ExpandProperty Content `
-| pup '.nmt-item a json{}' --plain `
-| ConvertFrom-Json -Depth 10 -AsHashtable `
-| ForEach-Object { $_.GetEnumerator() } `
+| ForEach-Object { $_ | pup 'a.nmt-item__link json{}' --plain } `
+| ConvertFrom-Json -Depth 10 `
+| ForEach-Object { $_ } `
 | ForEach-Object {
-    $Url = [regex]::Match($_.children[0].children[0].children[1].text, 'src="(.*)"').Groups[1].Value
-    $Body = "https://www.nrc.nl$($_.href)"
-    $DateElements = $_.href -split '/'
-    $Date = Get-Date -Year $DateElements[3] -Month $DateElements[4] -Day $DateElements[5]
-    $Title = 'Kamagurka {0:dddd d MMMM yyyy}' -f $Date
+    $Link = 'https://nrc.nl{0}' -f $_.href
+    $DateElements = ($Link -split '/')[4..6]
+    $Date = Get-Date -Year $DateElements[0] -Month $DateElements[1] -Day $DateElements[2]
+    $ImageUrl = (($_.children[0].children[0].children[0].'data-src') -split '\|')[1]
 
     Send-KibitzrNotification `
-        -Url $Url `
+        -Url $Link `
         -ApplicationToken as9kziktmqhy28bs287yuytno4iv85 `
         -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-        -Message $Body `
-        -Title $Title `
-        -ImageUrl $Url
+        -Message ('Kamagurka: {0:dddd d MMMM yyyy}' -f $Date) `
+        -ImageUrl $ImageUrl
 }
