144e4e557b27f1c4a0421404ca83d224eed6ee3f
Jan Hoek <github@uncommonsense.nl>
2020-12-04 13:55:10 +0100

    Use Send-KibitzrNotification

--- iplayer.ps1
+++ iplayer.ps1
@@ -1,13 +1,20 @@
 #!/usr/bin/env pwsh
+. ./Send-KibitzrNotification.ps1
 
 Find-AirTableRecord -BaseName appgyJZy5Dkjup0K4 -TableName Programmes `
-| ForEach-Object { Write-Host "Program $($_.Name)"; $_ } `
+| ForEach-Object { Write-Host "$($_.Name)" -ForegroundColor Cyan; $_ } `
 | ForEach-Object { & get_iplayer --pid-recursive-list "$($_.Url)" } `
 | Where-Object { $_ } `
 | Where-Object { $_ -notmatch '^Episodes:' } `
 | Where-Object { $_ -notmatch '^INFO:' } `
-| ForEach-Object { Write-Host "Found episode '$_'"; $_ } `
+| ForEach-Object { Write-Host "- $_"; $_ } `
 | ForEach-Object { $null = $_ -match '^(?<EpisodeTitle>.*), (?<Channel>.*), (?<ID>.*)$'; @{PID = $Matches.ID; Title = $Matches.EpisodeTitle } } `
-| Where-Object { -not (Test-AirTableRecord -BaseName appgyJZy5Dkjup0K4 -TableName Episodes -FieldName PID -Value ($_.PID)) } `
-| ForEach-Object { Send-PushoverNotification -ApplicationToken a4fo9r2dmbnjh8hk1m98zhich53e32 -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp -Message "New episode available: $($_.Title)"; $_ } `
-| ForEach-Object { New-AirTableRecord -BaseName appgyJZy5Dkjup0K4 -TableName Episodes -Fields $_ }
+| ForEach-Object {
+    Send-KibitzrNotification `
+        -Url $_.PID `
+        -UniqueID $_.PID `
+        -ApplicationToken a4fo9r2dmbnjh8hk1m98zhich53e32 `
+        -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+        -Message "New episode available: $($_.Title)" `
+        -TableName iplayer
+}
