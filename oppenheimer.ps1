2e4e5fd25ca7ecd0ec1a7d3c3728efd3be614be0
Jan Hoek <github@uncommonsense.nl>
2020-12-18 10:35:49 +0100

    Repaired/improved oppenheimer

--- oppenheimer.ps1
+++ oppenheimer.ps1
@@ -2,19 +2,21 @@
 . $PSScriptRoot/Send-KibitzrNotification.ps1
 
 Invoke-WebRequest -Uri 'https://www.nrc.nl/rubriek/ruben-l-oppenheimer/' `
-| Select-Object -ExpandProperty Content `
-| ForEach-Object { $_ | pup 'a.nmt-item__link json{}' --plain } `
-| ConvertFrom-Json -Depth 10 `
-| ForEach-Object { $_ } `
+| Select-Object -ExpandProperty Links `
+| Select-Object -ExpandProperty href `
+| Where-Object { $_ -like '/nieuws/*' } `
+| ForEach-Object { "https://www.nrc.nl$($_)" } `
 | ForEach-Object {
-    $Link = 'https://nrc.nl{0}' -f $_.href
-    $DateElements = ($Link -split '/')[4..6]
+    $DateElements = ($_ -split '/')[4..6]
     $Date = Get-Date -Year $DateElements[0] -Month $DateElements[1] -Day $DateElements[2]
-    $ImageUrl = (($_.children[0].children[0].children[0].'data-src') -split '\|')[1]
+
+    $ImageUrl =
+        Invoke-WebRequest -Uri $_ `
+        | Select-Object -ExpandProperty Content `
+        | ForEach-Object { $_ | pup 'img attr{src}' --plain }
 
     Send-KibitzrNotification `
-        -Url $Link `
-        -ApiKey keyL62zvsXw1vBbKZ `
+        -Url $_ `
         -ApplicationToken a7sw8gciyp7mn9ddgi9wcmbd15yk73 `
         -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
         -Message ('Ruben L. Oppenheimer {0:dddd d MMMM yyyy}' -f $Date) `
