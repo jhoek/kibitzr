7a646b9e026aeca157dbd8251e3e0efa81b55a19
Jan Hoek <github@uncommonsense.nl>
2021-01-15 15:19:19 +0100

    More work on teletekst; allow test items to be sent

--- Get-TextSimilarity.ps1
+++ Get-TextSimilarity.ps1
@@ -6,21 +6,21 @@ function Get-TextSimilarity
 {
     param
     (
-        [Parameter(Mandatory, Position=0)][string]$Text1,
-        [Parameter(Mandatory, Position=1)][string]$Text2
+        [Parameter(Mandatory, Position = 0)][string]$Text1,
+        [Parameter(Mandatory, Position = 1)][string]$Text2
     )
 
     $Words1 = $Text1 -split '[\s.,;]'
-    Write-Verbose "Words in Text1: $($Words1 -join ', ')"
+    Write-Debug "Words in Text1: $($Words1 -join ', ')"
 
     $Words2 = $Text2 -split '[\s.,;]'
-    Write-Verbose "Words in Text2: $($Words2 -join ', ')"
+    Write-Debug "Words in Text2: $($Words2 -join ', ')"
 
     $Union = Compare-Object $Words1 $Words2 -IncludeEqual -PassThru
-    Write-Verbose "Union: $($Union -join ', ')"
+    Write-Debug "Union: $($Union -join ', ')"
 
     $Intersection = Compare-Object $Words1 $Words2 -IncludeEqual -ExcludeDifferent -PassThru
-    Write-Verbose "Intersection: $($Intersection -join ', ')"
+    Write-Debug "Intersection: $($Intersection -join ', ')"
 
     $Intersection.Length / $Union.Length
 }
--- teletekst.ps1
+++ teletekst.ps1
@@ -1,6 +1,5 @@
 #!/usr/bin/pwsh
 . $PSScriptRoot/Get-TextSimilarity.ps1
-. $PSScriptRoot/Send-KibitzrNotification.ps1
 
 Import-Module UncommonSense.Teletekst
 
@@ -12,70 +11,92 @@ function ConvertTo-Base64
         [string]$Value
     )
 
-    [System.Convert]::ToBase64String(
-        [System.Text.Encoding]::Unicode.GetBytes($Value)
+    [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Value))
+}
+function Send-TeletekstNotification
+{
+    param
+    (
+        [ValidateNotNullOrEmpty()][string]$CachePath = "$PSScriptRoot/teletekst.json",
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)][string]$Title,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)][string]$Content,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)][string]$Link,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)][DateTime]$DateTime
     )
-}
 
-$CachedItems = @()
-$AddToCache = @()
-
-$Begin = {
-    if (Test-Path -Path $PSScriptRoot/teletekst.json)
+    begin
     {
-        $CachedItems =
-        Get-Content -Path $PSScriptRoot/teletekst.json
-        | ConvertFrom-Json -Depth 10
-        | Where-Object DateTime -GT (Get-Date).AddDays(-2)
-}
-}
+        $CachedItems = @()
+        $AddToCache = @()
 
-$Process = {
-    $CurrentItem = $_
-    $Hash = (ConvertTo-Base64 -Value "$($CurrentItem.Title) - $($CurrentItem.Content)")
-
-    if ($CachedItems | Where-Object Hash -EQ $Hash)
-    {
-        Write-Verbose "$($CurrentItem.Title) unchanged; skipping"
-        exit
-    }
-
-    $AnySimilar = $CachedItems.Where{
-        if ((Get-TextSimilarity $CurrentItem.Title $_.Title) -gt 0.7)
+        if (Test-Path -Path $CachePath)
         {
-            Write-Host "Title $($CurrentItem.Title) matches cached title $($_.Title)"
-            return $_
-        }
+            Write-Verbose "Using cache path $CachePath"
 
-        if ((Get-TextSimilarity $CurrentItem.Content $_.Content) -gt 0.7)
-        {
-            Write-Host "Content $($CurrentItem.Content) matches cached content $($_.Content)"
-            return $_
+            $CachedItems = Get-Content -Path $PSScriptRoot/teletekst.json |
+                ConvertFrom-Json -Depth 10 |
+                Where-Object DateTime -GT (Get-Date).AddDays(-2)
+
+            Write-Verbose "$($CachedItems.Length) items found in cache file."
         }
     }
 
-    if (-not($AnySimilar))
+    process
     {
+        $CurrentItem = $_
+        $CurrentItemAsText = "$($CurrentItem.Title) - $($CurrentItem.Content)"
+        $Hash = (ConvertTo-Base64 -Value $CurrentItemAsText)
+
+        if ($CachedItems | Where-Object Hash -EQ $Hash)
+        {
+            Write-Verbose "$($CurrentItem.Title) unchanged; skipping"
+            return
+        }
+
+        $CachedItems.ForEach{
+            $CurrentCachedItem = $_
+            $CurrentCachedItemAsText = "$($CurrentCachedItem.Title) - $($CurrentCachedItem.Content)"
+            $Similarity = Get-TextSimilarity $CurrentItemAsText $CurrentCachedItemAsText
+
+            if ($Similarity) -gt 0.7
+            {
+                # Send as update
+
+                $AddToCache = $AddToCache + [PSCustomObject]@{
+                    Title    = $CurrentItem.Title
+                    Content  = $CurrentItem.Content
+                    Hash     = $Hash
+                    DateTime = (Get-Date).ToString('s')
+                }
+
+                return
+            }
+        }
+
+        Write-Verbose "New (or substantially changed) item '$($CurrentItem.Title)'"
+
         Send-PushoverNotification `
             -ApplicationToken asxmmq8g95jt4ed1qcrucdvu2iuy67 `
             -Recipient gajrpycu8sq39dfbjn8ipjhypkhc7x `
             -Title $CurrentItem.Title `
             -Message $CurrentItem.Content `
             -SupplementaryUrl $CurrentItem.Link
+
+        $AddToCache = $AddToCache + [PSCustomObject]@{
+            Title    = $CurrentItem.Title
+            Content  = $CurrentItem.Content
+            Hash     = $Hash
+            DateTime = (Get-Date).ToString('s')
+        }
+
     }
 
-    $AddToCache = $AddToCache + [PSCustomObject]@{
-        Title    = $CurrentItem.Title
-        Content  = $CurrentItem.Content
-        Hash     = $Hash
-        DateTime = (Get-Date).ToString('s')
+    end
+    {
+        $CachedItems = $CachedItems + $AddToCache
+        $CachedItems | ConvertTo-Json -Depth 10 | Set-Content -Path $PSScriptRoot/teletekst.json
+
     }
 }
 
-$End = {
-    $CachedItems = $CachedItems + $AddToCache
-    $CachedItems | ConvertTo-Json -Depth 10 | Set-Content -Path $PSScriptRoot/teletekst.json
-}
-
-Get-TeletekstNews -Type Domestic, Foreign `
-| ForEach-Object -Begin $Begin -Process $Process -End $End -Verbose
+Get-TeletekstNews -Type Domestic, Foreign | Send-TeletekstNotification -Verbose
