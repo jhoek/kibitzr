e7c5d4a4e0dd76a27a07032e9b94f9a8d33c746e
Jan Hoek <github@uncommonsense.nl>
2021-01-23 12:51:34 +0100

    Finalized Jip van den Toorn

--- jipvandentoorn.ps1
+++ jipvandentoorn.ps1
@@ -1,6 +1,8 @@
 #!/usr/bin/pwsh
 . $PSScriptRoot/Send-KibitzrNotification.ps1
 
+$DutchCulture = New-Object -TypeName System.Globalization.CultureInfo -ArgumentList 'nl-NL'
+
 $Content = Invoke-WebRequest -Uri 'https://www.volkskrant.nl/cultuur-media/jip-van-den-toorn~b2e00b99/' `
 | Select-Object -ExpandProperty Content
 
@@ -8,30 +10,18 @@ $Dates = $Content
 | pup '.artstyle__main--container h3 text{}' --plain
 | ForEach-Object { $_.Trim() }
 | Where-Object { $_ }
-| ForEach-Object { "***$_***" }
+| ForEach-Object { [DateTime]::ParseExact($_, "d MMMM yyyy", $DutchCulture) }
 
 $Images = $Content
 | pup '.artstyle__main--container img attr{src}'
 
-$Dates.Count
-$IMages.Count
+$NoOfItems = ($Dates.Count), ($Images.Count) | Measure-Object -Minimum | Select-Object -ExpandProperty Minimum
 
-# | Select-Object -ExpandProperty href `
-# | Where-Object { $_ -like '/nieuws/*' } `
-# | ForEach-Object { "https://www.nrc.nl$($_)" } `
-# | ForEach-Object {
-#     $DateElements = ($_ -split '/')[4..6]
-#     $Date = Get-Date -Year $DateElements[0] -Month $DateElements[1] -Day $DateElements[2]
-
-#     $ImageUrl =
-#         Invoke-WebRequest -Uri $_ `
-#         | Select-Object -ExpandProperty Content `
-#         | ForEach-Object { $_ | pup 'img attr{src}' --plain }
-
-#     Send-KibitzrNotification `
-#         -Url $_ `
-#         -ApplicationToken a7sw8gciyp7mn9ddgi9wcmbd15yk73 `
-#         -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-#         -Message ('{0:dddd d MMMM yyyy}' -f $Date) `
-#         -ImageUrl $ImageUrl
-# }
+1..$NoOfItems | ForEach-Object {
+    Send-KibitzrNotification `
+        -Url $Images[$_] `
+        -ApplicationToken ayu9cm9t3f1satguceo6q5ifje821y `
+        -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+        -Message ('{0:dddd d MMMM yyyy}' -f $Dates[$_]) `
+        -ImageUrl $Images[$_]
+}
