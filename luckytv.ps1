cc46784725f8b895544e4ba4ae980833f3aa4b72
Jan Hoek <github@uncommonsense.nl>
2021-02-10 17:10:55 +0100

    Added luckytv

---
+++ luckytv.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/pwsh
+
+. $PSScriptRoot/Send-KibitzrNotification.ps1
+
+Invoke-WebRequest -Uri "https://www.evajinek.nl/node-in-term/ajax/252550/0" -SkipHttpErrorCheck
+| Where-Object StatusCode -eq 200
+| Select-Object -ExpandProperty Content
+| pup 'a attr{href}'
+| ForEach-Object {
+    $Content =
+    Invoke-WebRequest -Uri $_ -SkipHttpErrorCheck
+    | Where-Object StatusCode -eq 200
+    | Select-Object -ExpandProperty Content
+
+$Title = $Content | pup 'h1 text{}'
+$DateTime = ($Content | pup '.node-meta-attributes__time text{}') | Select-Object -First 1
+
+Send-KibitzrNotification `
+    -Url $_ `
+    -ApplicationToken afbzfvwfwjq5ritp51kw26sivnm1jj `
+    -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
+    -Title $Title `
+    -Message $DateTime
+}
