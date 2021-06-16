d65e312c1afce569bdcc9803d78d81e7b6cf7597
Jan Hoek <jhoek@uncommonsense.nl>
2021-06-16 20:31:44 +0200

    Added Verdienen & uitgeven

---
+++ verdienenenuitgeven.ps1
@@ -0,0 +1,18 @@
+#!/usr/bin/pwsh
+. $PSScriptRoot/Save-EntryToAirTable.ps1
+$ProgressPreference = 'SilentlyContinue'
+
+Invoke-WebRequest -Uri https://www.nrc.nl/rubriek/verdienen-en-uitgeven/ `
+| Select-Object -ExpandProperty Content `
+| pup '.compact-grid__item a attr{href}' --plain `
+| Select-Object -First 10 `
+| ForEach-Object {
+    $Url = "https://www.nrc.nl$($_)"
+    $DateText = (($Url -split '/')[4..6]) -join '-'
+    $Content = Invoke-WebRequest -Uri $Url | Select-Object -ExpandProperty Content
+    $Title = ($Content | pup 'h1[data-flowtype="headline"] text{}' --plain)
+    $Body = ($Content | pup 'div.content p text{}' --plain) -join ' '
+    $Date = [DateTime]::ParseExact($DateText, 'yyyy-MM-dd', $null)
+
+    Save-EntryToAirTable -Url $Url -Date $Date -Title "Verdienen en uitgeven: $Title" -Body $Body -TableName Mail
+}
