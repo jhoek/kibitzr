c00fd128c08a0e550e439d36a66c6542424e46f0
Jan Hoek <jhoek@uncommonsense.nl>
2021-07-01 17:58:58 +0200

    Updated Dirk-Jan url

--- dirkjan.ps1
+++ dirkjan.ps1
@@ -2,7 +2,7 @@
 
 . $PSScriptRoot/Send-KibitzrNotification.ps1
 
-Invoke-WebRequest -Uri https://www.parool.nl/kunst-media/dirkjan~bf34d4e2/ 
+Invoke-WebRequest -Uri https://www.parool.nl/ps/dirkjan~b8731189/
 | Select-Object -ExpandProperty Content
 | pup 'figure json{}' --plain
 | jq '[ .[].children[0].children[2] | { date: .\"data-title\", url: .\"data-original\" } ]'
