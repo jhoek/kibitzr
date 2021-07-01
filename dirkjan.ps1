e4da875793cb6b8ca4454018197a493fa1daaad4
Jan Hoek <jhoek@uncommonsense.nl>
2021-07-01 18:13:47 +0200

    Updated after site updates

--- dirkjan.ps1
+++ dirkjan.ps1
@@ -5,7 +5,7 @@
 Invoke-WebRequest -Uri https://www.parool.nl/ps/dirkjan~b8731189/
 | Select-Object -ExpandProperty Content
 | pup 'figure json{}' --plain
-| jq '[ .[].children[0].children[2] | { date: .\"data-title\", url: .\"data-original\" } ]'
+| jq '[ .[].children[0].children[0] | { date: .\"data-title\", url: .\"data-original\" } ]'
 | ConvertFrom-Json
 | ForEach-Object {
     Send-KibitzrNotification `
