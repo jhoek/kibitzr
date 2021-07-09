f7a081a5674350b04a61f79cc3225c9907eccc70
Jan Hoek <jhoek@uncommonsense.nl>
2021-07-09 11:16:25 +0200

    Script for downloading MagPi magazine as PDF

---
+++ magpi.ps1
@@ -0,0 +1,9 @@
+Invoke-WebRequest -Uri 'https://magpi.raspberrypi.org'
+| Select-Object -ExpandProperty Links
+| Where-Object href -Match '^/issues/(.*)/pdf$'
+| Select-Object -ExpandProperty href
+| ForEach-Object { Invoke-WebRequest "https://magpi.raspberrypi.org$_/download" }
+| Select-Object -ExpandProperty Links
+| Where-Object class -EQ 'c-link'
+| Select-Object -ExpandProperty href
+| ForEach-Object { Invoke-WebRequest "https://magpi.raspberrypi.org$_" -OutFile "~/Desktop/MagPi $($Matches[1]).pdf" }
