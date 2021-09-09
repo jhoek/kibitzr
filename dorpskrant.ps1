30fa7cafc346317e261964befb5424f39c9054c1
Jan Hoek <github@uncommonsense.nl>
2021-09-09 13:58:52 +0200

    Download dorpskrant

--- dorpskrant.ps1
+++ dorpskrant.ps1
@@ -2,20 +2,26 @@
 . $PSScriptRoot/Send-KibitzrNotification.ps1
 
 Invoke-WebRequest -Uri 'https://www.driemond.info/dorpskrant/'
-| Select-Object -Expand Content
-| pup '.j-downloadDocument json{}' --plain
-| jq '[.[]|{message:.children[1].children[0].text, link:.children[1].children[2].children[2].children[0].href}]'
-| ConvertFrom-Json
-| ForEach-Object {
-    $Url = "https://driemond.info$($($_.link))"
-    $UniqueID = $Url -replace '\?t=\d+$', ''
+| Select-Object -ExpandProperty Links
+| Select-Object -ExpandProperty href -Unique
+| Where-Object { $_ -match "/app/download/\d+/([^?]+)\?" }
+| Select-Object -First 1
+| ForEach-Object { Invoke-WebRequest -Uri "https://www.driemond.info$_" -OutFile "/shared/$Matches[1]" }
 
-    Send-KibitzrNotification `
-        -Url $Url `
-        -UniqueID $UniqueID `
-        -ApplicationToken a53g88zpsxd8srdx5ufzu9aq1w73gd `
-        -Recipient gd3q14vf9sq3nhj7tqt7icrqpojwcv `
-        -Title $_.message `
-        -Message $Url `
-        -TableName dorpskrant
-}
+# | ForEach-Object { Invoke-WebRequest -Uri "https://www.driemond.info$_" -OutFile }
+# | pup '.j-downloadDocument json{}' --plain
+# | jq '[.[]|{message:.children[1].children[0].text, link:.children[1].children[2].children[2].children[0].href}]'
+# | ConvertFrom-Json
+# | ForEach-Object {
+#     $Url = "https://driemond.info$($($_.link))"
+#     $UniqueID = $Url -replace '\?t=\d+$', ''
+
+#     Send-KibitzrNotification `
+#         -Url $Url `
+#         -UniqueID $UniqueID `
+#         -ApplicationToken a53g88zpsxd8srdx5ufzu9aq1w73gd `
+#         -Recipient gd3q14vf9sq3nhj7tqt7icrqpojwcv `
+#         -Title $_.message `
+#         -Message $Url `
+#         -TableName dorpskrant
+# }
