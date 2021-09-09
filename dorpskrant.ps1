667cb55ebe5aadf5ea2302dc86c403420e62bce4
Jan Hoek <github@uncommonsense.nl>
2021-09-09 14:05:23 +0200

    Removed commented out code

--- dorpskrant.ps1
+++ dorpskrant.ps1
@@ -7,21 +7,3 @@ Invoke-WebRequest -Uri 'https://www.driemond.info/dorpskrant/'
 | Where-Object { $_ -match "/app/download/\d+/([^?]+)\?" }
 | Select-Object -First 1
 | ForEach-Object { Invoke-WebRequest -Uri "https://www.driemond.info$_" -OutFile "/shared/$($Matches[1])" }
-
-# | ForEach-Object { Invoke-WebRequest -Uri "https://www.driemond.info$_" -OutFile }
-# | pup '.j-downloadDocument json{}' --plain
-# | jq '[.[]|{message:.children[1].children[0].text, link:.children[1].children[2].children[2].children[0].href}]'
-# | ConvertFrom-Json
-# | ForEach-Object {
-#     $Url = "https://driemond.info$($($_.link))"
-#     $UniqueID = $Url -replace '\?t=\d+$', ''
-
-#     Send-KibitzrNotification `
-#         -Url $Url `
-#         -UniqueID $UniqueID `
-#         -ApplicationToken a53g88zpsxd8srdx5ufzu9aq1w73gd `
-#         -Recipient gd3q14vf9sq3nhj7tqt7icrqpojwcv `
-#         -Title $_.message `
-#         -Message $Url `
-#         -TableName dorpskrant
-# }
