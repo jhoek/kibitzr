5942bae0401117aec0bb14cb37772df79580186a
Jan Hoek <jhoek@uncommonsense.nl>
2021-09-12 17:38:24 +0200

    Use history for dorpskrant

--- dorpskrant.ps1
+++ dorpskrant.ps1
@@ -1,9 +1,10 @@
 #!/usr/bin/env pwsh
 . $PSScriptRoot/Send-KibitzrNotification.ps1
+. $PSScriptRoot/Test-PreviouslyUnseen.ps1
 
 Invoke-WebRequest -Uri 'https://www.driemond.info/dorpskrant/'
 | Select-Object -ExpandProperty Links
 | Select-Object -ExpandProperty href -Unique
 | Where-Object { $_ -match "/app/download/\d+/([^?]+)\?" }
-| Select-Object -First 1
+| Where-Object { $_ | Test-PreviouslyUnseen -Path ~/dorpskrant.seen }
 | ForEach-Object { Invoke-WebRequest -Uri "https://www.driemond.info$_" -OutFile "/shared/$($Matches[1])" }
