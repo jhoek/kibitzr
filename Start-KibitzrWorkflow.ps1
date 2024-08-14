c1572da862c600cc5438ef902abb9e82b0ff9ac0
Jan Hoek <jhoek@uncommonsense.nl>
2024-08-14 22:38:06 +0200

    Self-hosted ftw

--- Start-KibitzrWorkflow.ps1
+++ Start-KibitzrWorkflow.ps1
@@ -48,8 +48,6 @@ function Update-RssFeed
 Push-Location ~/GitHub/kibitzr
 try
 {
-    Update-RssFeed -Source { Get-EvaHoeke } -Title 'Eva Hoeke' -Link 'https://www.volkskrant.nl/auteur/eva-hoeke' -Destination './data/evahoeke.xml'
-    Update-RssFeed -Source { Get-IonicaSmeets } -Title 'Ionica Smeets' -Link 'https://www.volkskrant.nl/auteur/ionica-smeets' -Destination './data/ionicasmeets.xml'
     Update-RssFeed -Source { Get-ThomasVanLuyn } -Title 'Thomas van Luyn' -Link 'https://www.volkskrant.nl/auteur/thomas-van-luyn' -Destination './data/thomasvanluyn.xml'
     Update-RssFeed -Source { Get-JipVanDenToorn } -Title 'Jip van den Toorn' -Link 'https://www.volkskrant.nl/cartoons/jip-van-den-toorn~bbe9994c/' -Destination './data/jipvandentoorn.xml' -Cartoon
     Update-RssFeed -Source { Get-BasVanDerSchot } -Title 'Bas van der Schot' -Link 'https://www.volkskrant.nl/cartoons/bas-van-der-schot~b31a8d34/' -Destination './data/basvanderschot.xml' -Cartoon
