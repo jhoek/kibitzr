b3174ba717405d895e42bcace3a57b4b16afd765
Jan Hoek <jhoek@uncommonsense.nl>
2024-08-14 22:34:29 +0200

    Self-hosted ftw

--- Start-KibitzrWorkflow.ps1
+++ Start-KibitzrWorkflow.ps1
@@ -48,9 +48,7 @@ function Update-RssFeed
 Push-Location ~/GitHub/kibitzr
 try
 {
-    Update-RssFeed -Source { Get-BertKeizer } -Title 'Bert Keizer' -Link 'https://www.trouw.nl/auteur/bert-keizer' -Destination './data/bertkeizer.xml'
     Update-RssFeed -Source { Get-TienGeboden } -Title 'Tien Geboden' -Link 'https://www.trouw.nl/dossier-tien-geboden' -Destination './data/tiengeboden.xml'
-    Update-RssFeed -Source { Get-TeunVanDeKeuken } -Title 'Teun van de Keuken' -Link 'https://www.volkskrant.nl/auteur/teun-van-de-keuken' -Destination './data/teunvandekeuken.xml'
     Update-RssFeed -Source { Get-SylviaWitteman } -Title 'Sylvia Witteman' -Link 'https://www.volkskrant.nl/auteur/Sylvia%20Witteman' -Destination './data/sylviawitteman.xml'
     Update-RssFeed -Source { Get-EvaHoeke } -Title 'Eva Hoeke' -Link 'https://www.volkskrant.nl/auteur/eva-hoeke' -Destination './data/evahoeke.xml'
     Update-RssFeed -Source { Get-IonicaSmeets } -Title 'Ionica Smeets' -Link 'https://www.volkskrant.nl/auteur/ionica-smeets' -Destination './data/ionicasmeets.xml'
